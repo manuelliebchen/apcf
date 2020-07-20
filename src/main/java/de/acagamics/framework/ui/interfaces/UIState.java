6a8d328705db3610999f1fc554b11c2a76150d04
Manuel Liebchen <hallo@manuelliebchen.de>
2020-07-20 22:55:00 +0200

    adding a tansformation calculation function to UIState

--- src/main/java/de/acagamics/framework/ui/interfaces/UIState.java
+++ src/main/java/de/acagamics/framework/ui/interfaces/UIState.java
@@ -4,8 +4,10 @@ import de.acagamics.framework.ui.StateManager;
 import de.acagamics.framework.ui.elements.Background;
 import de.acagamics.framework.ui.elements.RenderingLayer;
 import javafx.event.EventHandler;
+import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.input.InputEvent;
+import javafx.scene.transform.Affine;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -70,4 +72,22 @@ public abstract class UIState implements EventHandler<InputEvent> {
 			button.handle(event);
 		}
 	}
+
+	/**
+	 * Calculates a transformation that moves the zero vector to the center of the screen and scales it so that minLength is the smaller dimention of the canvas.
+	 */
+	public static Affine calcultateTransformation(Canvas canvas, float minLenght) {
+		Affine transformation = new Affine();
+		transformation.appendTranslation(canvas.getWidth() / 2, canvas.getHeight() / 2);
+		float scalation = 1;
+		if (canvas.getHeight() > canvas.getWidth()) {
+			scalation = (float) canvas.getWidth() / minLenght;
+		} else {
+			scalation = (float) canvas.getHeight() / minLenght;
+		}
+		transformation.appendScale(scalation,
+				scalation);
+		return transformation;
+	}
+
 }
